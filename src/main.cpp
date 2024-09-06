c6d46ddd182f054847ea7c6d2825f0feacbb9929
Aldi Sugiarto <aldi.sugiarto@bluebirdgroup.com>
2024-09-06 10:04:12 +0700

    initial project

---
+++ src/main.cpp
@@ -0,0 +1,59 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "your-wifi-ssid";
+const char* password = "your-wifi-password";
+const char* mqtt_server = "your-mqtt-broker";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect("ESP32Client")) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  String payload = String("{\"temperature\":") + random(20, 30) + ",\"humidity\":" + random(50, 60) + "}";
+  client.publish("sensor/data", payload.c_str());
+  delay(5000);
+}
